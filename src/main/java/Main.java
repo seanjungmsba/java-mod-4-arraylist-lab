19998bcaaffbef39292305281092806b1128b1f0
seanjungmsba <seanjeong1996@gmail.com>
2022-07-18 14:10:54 -0400

    Commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,6 +15,10 @@ public class Main {
 
     public static ArrayList<Integer> mergeTwoArrayLists(ArrayList<Integer> l1, ArrayList<Integer> l2) {
         // your code here
-        return null;
+        ArrayList<Integer> combinedList = (ArrayList<Integer>)Stream.of(l1, l2)
+                                                                    .flatMap(x -> x.stream())
+                                                                    .filter(i -> i >= 0)
+                                                                    .collect(Collectors.toList());
+        return combinedList; // [4, 13, 5, 7, 3, 5, 10, 8]
     }
 }
